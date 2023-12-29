e79c062a3b02a6e2cb629245e28cc0cd181d30ac
Tasadar <106964192+Tasadar2003@users.noreply.github.com>
2023-12-29 17:19:51 +0100

    v2.1
    - Moddified function get_category
    - Rename to get_json_from_api

--- Django_test/catalog/api_request.py
+++ Django_test/catalog/api_request.py
@@ -4,12 +4,19 @@ import requests
 URL = 'http://127.0.0.1:8000/api/category/?format=json'
 
 
-def get_category():
-    req = requests.get(URL)
-    print(req.status_code)
-    if req.status_code == 200:
-        for item in req.json():
-            print(item)
+def get_json_from_api(URL):
+    try:
+        req = requests.get(URL)
+        if req.status_code == 200:
+            print(f"Status: code {req.status_code}")
+            for item in req.json():
+                print(item)
+        else:
+            print(f"Error receiving data from the API. Status code: {req.status_code}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
 
 
-get_category()
+json_data= get_json_from_api(URL)
